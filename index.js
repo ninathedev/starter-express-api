8dc4e6045ae0a8c902cb92d1a909e99d71728370
ninathedev <realguybackup@gmail.com>
2024-03-18 06:57:43 +0800

    shdgfadsajdsjfasdfasgesggfafsgufugadshfakseyyg

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ app.get('/', (req, res) => {
     res.sendFile('./public/mainPage/index.html', { root: __dirname });
 });
 
+app.get('/login', (req, res) => {
+    if (req.body.fHeusGF === 'TUFDSElDQQ==') {
+
+    }
+    const con = mysql.createConnection({
+        host: 'emerald-free.falixserver.net:3306',
+        user: 'u1332246_4Ign2Pc0qA',
+        password: '4fcSn+h8ZL71lLSdHT8d^5H+',
+        database: 's1332246_27f8817bffaf'
+    });
+    con.connect(function(err) {
+        if (err) throw err;
+        // id = ${fHeusGF}
+        // pin = ${hDjeRfg}
+        var sql = ``;
+
+        con.query(sql, function (err, result) {
+            if (err) app.send(err);
+            app.send("done")
+        });
+    });
+
+
+})
+
 app.post('/surequest', (req, res) => {
     const con = mysql.createConnection({
         host: 'emerald-free.falixserver.net:3306',
