1ebe1c7cd4ade6acca4ab77dfc4283600cc99659
ninathedev <realguybackup@gmail.com>
2024-04-01 21:18:54 +0800

    Revert "u"
    This reverts commit d9ca92880958d90391f1767c5d406589a51e72bd.

--- index.mjs
+++ index.mjs
@@ -3,49 +3,48 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import axios from 'axios';
-import express, { Application } from 'express';
-const app: Application = express();
+import express from 'express';
+const app = express();
 import mysql from 'mysql';
 import geoip from 'geoip-lite';
 import path from 'path';
-import { Request, Response } from 'express';
 
-const __dirname: string = path.resolve();
+const __dirname = path.resolve();
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req, res) => {
 	res.sendFile('./public/mainPage/index.html', {root: __dirname});
 });
 
-app.get('/scripts/mindex.js', (req: Request, res: Response) => {
+app.get('/scripts/mindex.js', (req, res) => {
 	res.sendFile('./scripts/mindex.js', {root: __dirname});
 });
-app.get('/scripts/aindex.js', (req: Request, res: Response) => {
+app.get('/scripts/aindex.js', (req, res) => {
 	res.sendFile('./scripts/aindex.js', {root: __dirname});
 });
-app.get('/scripts/dindex.js', (req: Request, res: Response) => {
+app.get('/scripts/dindex.js', (req, res) => {
 	res.sendFile('./scripts/dindex.js', {root: __dirname});
 });
 
-app.get('/money', (req: Request, res: Response) => {
-	const con: mysql.Connection = mysql.createConnection({
+app.get('/money', (req, res) => {
+	const con = mysql.createConnection({
 		host: process.env.MYSQLIP,
 		user: process.env.MYSQLUSER,
 		password: process.env.MYSQLPW,
 		database: process.env.MYSQLDB
 	});
 
-	con.connect((err: Error) => {
+	con.connect(function(err) {
 		if (err) throw err;
-		const sql: String = 'SELECT * FROM money;';
-		con.query(sql, (err: Error, result: Array<JSON>) => {
+		const sql = 'SELECT * FROM money;';
+		con.query(sql, function(err, result) {
 			if (err) res.status(500).send(err);
 			res.status(200).send(result);
 		});
 	});
 });
 
-app.put('/transact', (req: Request, res: Response) => {
+app.put('/transact', (req, res) => {
 	if (!req.body.from || !req.body.to || !req.body.amount) {
 		res.status(400).send('No ID provided');
 		return;
@@ -54,15 +53,15 @@ app.put('/transact', (req: Request, res: Response) => {
 		res.status(400).send('No amount given');
 		return;
 	}
-	const con: mysql.Connection = mysql.createConnection({
+	const con = mysql.createConnection({
 		host: process.env.MYSQLIP,
 		user: process.env.MYSQLUSER,
 		password: process.env.MYSQLPW,
 		database: process.env.MYSQLDB
 	});
-	con.connect(function(err: Error) {
+	con.connect(function(err) {
 		if (err) throw err;
-		con.query(`UPDATE money SET money = money + ${req.body.amount} WHERE id = ${req.body.to};`, (err: Error, result: Array<JSON>) => {
+		con.query(`UPDATE money SET money = money + ${req.body.amount} WHERE id = ${req.body.to};`, function(err, result) {
 			if (err) {
 				console.log(err);
 				res.status(500).send(err);
@@ -79,7 +78,7 @@ app.put('/transact', (req: Request, res: Response) => {
 			res.status(204);
 		});
 	});
-	axios.post('https://discord.com/api/webhooks/1223843361627181136/qAuiq7tLEaIjeoCJIXQ3WRImZw-A-w1AQ2eVpDBEAkQXd0O-8bi5VtEd1gcybdGebiGB', {
+	axios.post(process.env.WHTRANS, {
 		embeds: [{
 			title: `Transaction from ${req.body.from} to ${req.body.to}`,
 			description: `Amount: ${req.body.amount}`,
@@ -88,11 +87,11 @@ app.put('/transact', (req: Request, res: Response) => {
 	});
 });
 
-app.get('/FAQ', (req: Request, res: Response) => {
+app.get('/FAQ', (req, res) => {
 	res.sendFile('./public/FAQ/index.html', {root: __dirname});
 });
 
-app.put('/insal', (req: Request, res: Response) => {
+app.put('/insal', (req, res) => {
 	if (!req.body.id || !req.body.salary) {
 		res.status(400).send('No ID provided');
 		return;
@@ -101,16 +100,16 @@ app.put('/insal', (req: Request, res: Response) => {
 		res.status(400).send('No amount given');
 		return;
 	}
-	const con: mysql.Connection = mysql.createConnection({
+	const con = mysql.createConnection({
 		host: process.env.MYSQLIP,
 		user: process.env.MYSQLUSER,
 		password: process.env.MYSQLPW,
 		database: process.env.MYSQLDB
 	});
-	con.connect((err: Error) => {
+	con.connect(function(err) {
 		if (err) throw err;
-		const sql: String = `UPDATE money SET money = money + ${req.body.salary} WHERE id = ${req.body.id}`;
-		con.query(sql, (err: Error, result: Array<JSON>) => {
+		const sql = `UPDATE money SET money = money + ${req.body.salary} WHERE id = ${req.body.id}`;
+		con.query(sql, function(err, result) {
 			if (err) {
 				console.log(err);
 				res.status(500).send(err);
@@ -138,25 +137,25 @@ app.put('/insal', (req: Request, res: Response) => {
 	});
 });
 
-app.get('/accounts', (req: Request, res: Response) => {
-	const con: mysql.Connection = mysql.createConnection({
+app.get('/accounts', (req, res) => {
+	const con = mysql.createConnection({
 		host: process.env.MYSQLIP,
 		user: process.env.MYSQLUSER,
 		password: process.env.MYSQLPW,
 		database: process.env.MYSQLDB
 	});
 
-	con.connect((err: Error) => {
+	con.connect(function(err) {
 		if (err) throw err;
-		const sql: String = 'SELECT * FROM accounts;';
-		con.query(sql, (err: Error, result: Array<JSON>) => {
+		const sql = 'SELECT * FROM accounts;';
+		con.query(sql, function(err, result) {
 			if (err) res.status(500).send(err);
 			res.status(200).send(result);
 		});
 	});
 });
 
-app.get(`/${process.env.ADMIN_PIN}/admin`, (req: Request, res: Response) => {
+app.get(`/${process.env.ADMIN_PIN}/admin`, (req, res) => {
 	axios.post(process.env.WHADMIN, {
 		embeds: [{
 			title: 'Admin accessed',
@@ -170,26 +169,26 @@ ${JSON.stringify(geoip.lookup(req.headers['x-forwarded-for'] || req.socket.remot
 	res.sendFile('./public/admin/index.html', {root: __dirname});
 });
 
-app.get('/dashboard', (req: Request, res: Response) => {
+app.get('/dashboard', (req, res) => {
 	res.sendFile('./public/dashboard/index.html', {root: __dirname});
 });
 
-app.put('/tax', (req: Request, res: Response) => {
+app.put('/tax', (req, res) => {
 	if (req.body.tax === 0 || !req.body.tax) {
 		res.status(400).send('No amount given');
 		return;
 	} 
-	const con: mysql.Connection = mysql.createConnection({
+	const con = mysql.createConnection({
 		host: process.env.MYSQLIP,
 		user: process.env.MYSQLUSER,
 		password: process.env.MYSQLPW,
 		database: process.env.MYSQLDB
 	});
 
-	con.connect((err: Error) => {
+	con.connect(function(err) {
 		if (err) throw err;
-		const sql: String = `UPDATE money SET money = money - ${req.body.tax}`;
-		con.query(sql, (err: Error, result: Array<JSON>) => {
+		const sql = `UPDATE money SET money = money - ${req.body.tax}`;
+		con.query(sql, function(err, result) {
 			if (err) res.status(500).send(err);
 			res.status(206).send(result);
 			if (req.body.tax < 0) {
@@ -213,23 +212,23 @@ app.put('/tax', (req: Request, res: Response) => {
 	});
 });
 
-app.post('/login', (req: Request, res: Response) => {
+app.post('/login', (req, res) => {
 	if (!req.body.fHeusGF && !req.body.hDjeRfg) res.status(404).send('No credientials provided');
 	if (req.body.fHeusGF === process.env.ADMIN_ID && req.body.hDjeRfg === process.env.ADMIN_PIN) {
 		res.status(301);
 		return;
 	}
-	const con: mysql.Connection = mysql.createConnection({
+	const con = mysql.createConnection({
 		host: process.env.MYSQLIP,
 		user: process.env.MYSQLUSER,
 		password: process.env.MYSQLPW,
 		database: process.env.MYSQLDB
 	});
-	con.connect((err: Error) => {
+	con.connect(function(err) {
 		if (err) throw err;
-		const sql: String = `SELECT * FROM accounts WHERE id = ${req.body.fHeusGF} AND pin = ${req.body.hDjeRfg};`;
+		const sql = `SELECT * FROM accounts WHERE id = ${req.body.fHeusGF} AND pin = ${req.body.hDjeRfg};`;
 
-		con.query(sql, (err: Error, result: Array<JSON>) => {
+		con.query(sql, (err, result) => {
 			if (result.length === 0) {
 				res.status(404);
 				axios.post(process.env.WHLOGIN, {
@@ -264,28 +263,28 @@ ${JSON.stringify(geoip.lookup(req.headers['x-forwarded-for'] || req.socket.remot
 	
 });
 
-app.get('/404', (req: Request, res: Response) => {
+app.get('/404', (req, res) => {
 	res.sendFile('./public/404/index.html', {root: __dirname});
 });
 
-app.post('/mysql', (req: Request, res: Response) => {
-	let loginMode: Boolean = false;
+app.post('/mysql', (req, res) => {
+	let loginMode = false;
 	if (!req.body.sql) {
 		res.status(400).send('No SQL query provided');
 		return;
 	} else if (req.body.login) {
 		loginMode = true;
 	}
-	const con: mysql.Connection = mysql.createConnection({
+	const con = mysql.createConnection({
 		host: process.env.MYSQLIP,
 		user: process.env.MYSQLUSER,
 		password: process.env.MYSQLPW,
 		database: process.env.MYSQLDB
 	});
-	con.connect((err: Error) => {
+	con.connect(function(err) {
 		if (err) console.log(err);
-		const sql: String = req.body.sql;
-		con.query(sql, (err: Error, result: Array<JSON>) => {
+		const sql = req.body.sql;
+		con.query(sql, function(err, result) {
 			if (err) {
 				if (!loginMode) {
 					axios.post(process.env.WHMYSQL, {
@@ -339,7 +338,7 @@ app.post('/mysql', (req: Request, res: Response) => {
 
 
 // Handle 404
-app.use(function(req: Request, res: Response) {
+app.use(function(req, res) {
 	res.status(404).sendFile('./public/404/index.html', {root: __dirname});
 });
 
