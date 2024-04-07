f169ce536bb4d32089b44c9f7c3e70cd58f99a30
ninathedev <realguybackup@gmail.com>
2024-04-07 15:15:27 +0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -362,7 +362,7 @@ const list = [{
 	colors: ['#FFFFFF']
 }];
 
-const currentPalette = 4;
+const currentPalette = 2;
 
 function rgbToHex(r, g, b) {
 	const hex = ((r << 16) | (g << 8) | b).toString(16).toUpperCase();
